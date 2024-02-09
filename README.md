ee4831df997dae9f66b16e49d4d1d73c6e329144
shizuoka <deadshizuko@gmail.com>
2024-02-09 03:56:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+
+Pow Generate
+
+Script that generates solana wallets and writes the private key to a file if the address starts with "pow + digit"
+
+
+How to use:
+
+>> - download Golang https://go.dev/doc/install
+
+console commands:
+
+- go mod download 
+
+- go run . 
+
+if you want to compile code 
+
+- go build main.go
+
+- ./main
+
+mne vashe pohui.
+
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	sol "github.com/gagliardetto/solana-go"
+)
+
+func main() {
+	for index := 0; index >= 0; index++ {
+		x := sol.NewWallet()
+		fmt.Printf("%v Address: %v | %v Private: %v | HUESOSI PLEASE GIVE ME SOL\n", index, x.PublicKey(), index, x.PrivateKey)
+
+		if x.PublicKey().String()[:3] == "pow" {
+			fileWrite(x.PublicKey().String()[:10], []byte(x.PrivateKey))
+		}
+	}
+}
+
+func fileWrite(filename string, data []byte) {
+	file, _ := os.Create(filename + ".json")
+	defer file.Close()
+
+	str := makeStr(data)
+
+	_, err := file.WriteString(str.String())
+	if err != nil {
+		fmt.Println("Ошибка при записи в файл:", err)
+		return
+	}
+
+}
+
+func makeStr(data []byte) strings.Builder {
+	var str strings.Builder
+	str.WriteString("[")
+	for i, num := range data {
+		if i != 0 {
+			str.WriteString(",")
+		}
+		str.WriteString(fmt.Sprintf("%d", num))
+	}
+	str.WriteString("]")
+
+	return str
+}
